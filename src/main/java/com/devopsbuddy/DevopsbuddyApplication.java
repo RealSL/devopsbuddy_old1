9c853469ce88425b4842af08d0046dd1931ef177
SL <shengboliu@yahoo.com>
2017-12-30 11:45:12 -0600

    Added Logout logging

--- src/main/java/com/devopsbuddy/DevopsbuddyApplication.java
+++ src/main/java/com/devopsbuddy/DevopsbuddyApplication.java
@@ -1,12 +1,17 @@
 package com.devopsbuddy;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.devopsbuddy.web.i18n.I18NService;
 
 @SpringBootApplication
 public class DevopsbuddyApplication {
 
 	public static void main(String[] args) {
+		/** The application logger */
 		SpringApplication.run(DevopsbuddyApplication.class, args);
 	}
 }
--- src/main/java/com/devopsbuddy/web/i18n/I18NService.java
+++ src/main/java/com/devopsbuddy/web/i18n/I18NService.java
@@ -2,14 +2,21 @@ package com.devopsbuddy.web.i18n;
 
 import java.util.Locale;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Service;
 
+
+
 @Service
 public class I18NService {
 
+	/** The application logger */
+	private static final Logger LOG=LoggerFactory.getLogger(I18NService.class);
+	
 	@Autowired
 	private MessageSource messageSource;
 	
@@ -18,7 +25,10 @@ public class I18NService {
 	 * @parammessageId The key to the messages resource file
 	 **/
 	public String getMessage(String messageId) {
+		LOG.info("Returning i18n text for messageId {}", messageId);
+		
 		Locale locale = LocaleContextHolder.getLocale();
+		
 		return getMessage(messageId, locale);
 	}
 	
@@ -28,6 +38,8 @@ public class I18NService {
 	 * @param locale The Locale
 	 **/
 	public String getMessage(String messageId, Locale locale) {
+		LOG.info("Returning i18n text for messageId {}", messageId);
+
 		return messageSource.getMessage(messageId, null, locale);
 	}
 	
